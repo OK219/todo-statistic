46bde2b12fc355183cb051256f58dd00015e0c23
OK <143250301+OK219@users.noreply.github.com>
2025-03-04 17:53:22 +0500

    showTODO importantTODO

--- index.js
+++ index.js
@@ -16,10 +16,40 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            showTODO();
+            break;
+        case 'important':
+            showImportantTODO();
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getAllTODOs() {
+    const todos = [];
+    files.forEach(file => {
+        const lines = file.split('\n');
+        lines.forEach(line => {
+            if (line.includes('// TODO')) {
+                todos.push(line.trim());
+            }
+        });
+    });
+    return todos;
+}
+
+function showTODO() {
+    const todos = getAllTODOs();
+    todos.forEach(todo => console.log(todo));
+}
+
+function showImportantTODO() {
+    const todos = getAllTODOs();
+    const importantTodos = todos.filter(todo => todo.includes('!'));
+    importantTodos.forEach(todo => console.log(todo));
 }
 
 // TODO you can do it!
